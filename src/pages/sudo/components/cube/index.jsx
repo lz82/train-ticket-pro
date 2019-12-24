bb0779bbe9d14b4060d495713f87def32e79a1db
liuzhuang <70458055@qq.com>
2019-12-24 18:40:12 +0800

    :construction: complete basic function

--- src/pages/sudo/components/cube/index.jsx
+++ src/pages/sudo/components/cube/index.jsx
@@ -1,31 +1,104 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Icon } from 'antd'
 
 import styles from './index.module.less'
 
 function Cube(props) {
-  let index = props.item.key
-  let rightCls = ''
-  let bottomCls = ''
-  if(index % 4 === 1) {
-    console.log(index, '4')
-    rightCls = 'border-right-4'
-  } else if (index % 4 === 0 || index % 4 === 2) {
-    console.log(index, '1')
-    rightCls = 'border-right-1'
-  } else {
-    console.log(index, '0')
-    rightCls = 'border-right-0'
-  }
-  const temp = parseInt(index / 4, 10)
-  if (temp === 1) {
-    bottomCls = 'border-bottom-4'
-  } else if (temp === 0 || temp === 2) {
-    bottomCls = 'border-bottom-1'
-  } else {
-    bottomCls = 'border-bottom-0'
+  const [showOp, setShowOp] = useState(false)
+  const [style, setStyle] = useState({})
+  const [cube, setCube] = useState(props.item)
+
+  const handleMouseOver = () => {
+    setShowOp(true)
   }
 
-  return <div className={[styles['cube-wrapper'], styles[rightCls], styles[bottomCls]].join(' ')}>cube</div>
+  const handleMouseOut = () => {
+    setShowOp(false)
+  }
+  const calcStyle = () => {
+    let index = cube.key
+    let rightCls = ''
+    let bottomCls = ''
+    if (index % 4 === 1) {
+      rightCls = 'border-right-4'
+    } else if (index % 4 === 0 || index % 4 === 2) {
+      rightCls = 'border-right-1'
+    } else {
+      rightCls = 'border-right-0'
+    }
+    const temp = parseInt(index / 4, 10)
+    if (temp === 1) {
+      bottomCls = 'border-bottom-4'
+    } else if (temp === 0 || temp === 2) {
+      bottomCls = 'border-bottom-1'
+    } else {
+      bottomCls = 'border-bottom-0'
+    }
+    return {
+      rightCls,
+      bottomCls
+    }
+  }
+
+  const handleChange = (add) => {
+    let temp = cube.val + add
+    if (temp > 4) temp = 4
+    if (temp < 1) temp = 1
+    setCube({
+      key: cube.key,
+      val: temp,
+      isLock: cube.isLock
+    })
+    props.onChange({
+      key: cube.key,
+      val: temp
+    })
+  }
+
+  useEffect(() => {
+    setStyle(calcStyle())
+  }, [])
+
+  useEffect(() => {
+    console.log('useEffect...')
+    setCube(props.item)
+    renderData()
+  }, [props.item.val, props.item.hasError])
+
+  const renderData = () => {
+    return (
+      <div
+        className={[
+          styles['cube-wrapper'],
+          styles[style.rightCls],
+          styles[style.bottomCls],
+          cube.hasError ? styles['error'] : ''
+        ].join(' ')}
+        onMouseEnter={handleMouseOver}
+        onMouseLeave={handleMouseOut}
+      >
+        {cube.isLock ? <i className={'iconfont icon-lock ' + styles['lock-icon']} /> : null}
+        <span>{cube.val}</span>
+
+        {!cube.isLock && showOp ? (
+          <>
+            <Icon
+              type="plus-circle"
+              className={styles['arrow-top']}
+              onClick={() => handleChange(1)}
+            />
+            <Icon
+              type="minus-circle"
+              className={styles['arrow-down']}
+              onClick={() => handleChange(-1)}
+            />
+          </>
+        ) : null}
+      </div>
+    )
+  }
+
+  return renderData()
 }
 
 export default Cube
--- src/pages/sudo/index.jsx
+++ src/pages/sudo/index.jsx
@@ -1,26 +1,157 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styles from './index.module.less'
-
+import { Button } from 'antd'
 import Cube from './components/cube'
 
+const sudoLib = [
+  [3, 1, null, 4, null, null, 1, null, null, 2, null, null, null, 3, null, 2],
+  [null, 1, null, null, null, null, 1, 2, null, 2, null, null, 1, null, 2, 4],
+  [1, null, 2, null, null, null, 4, null, 4, null, null, 2, null, null, 3, null],
+  [null, 4, 2, null, null, 3, null, null, null, null, 4, 2, 4, null, null, 1],
+  [null, 3, null, 4, 1, null, 2, null, 3, null, 4, 1, null, 1, null, null],
+  [2, null, 4, null, null, null, 3, null, 3, null, null, 4, null, 2, 1, null],
+  [null, 3, 2, null, null, null, 3, null, 3, 4, null, null, 1, null, null, 3],
+  [null, null, 4, null, null, 2, 1, null, null, null, null, null, 1, 3, null, 4],
+  [null, 3, null, 1, null, 1, null, 2, 1, null, null, null, null, null, 1, 4],
+  [4, null, null, 2, null, 3, 4, null, null, 4, null, 3, 3, null, null, null],
+  [null, 2, null, null, 4, null, null, 3, null, 3, 1, null, null, 4, 3, null],
+  [null, null, 3, 4, null, null, null, null, 2, 4, 1, null, null, 1, null, null],
+  [1, null, null, 4, null, 4, 1, null, null, 3, 2, null, 2, null, null, 3],
+  [1, null, 3, null, null, 3, null, null, null, 2, null, 3, null, null, null, 4]
+]
+
+// 检查行是否有重复
+const rows = [
+  [0, 1, 2, 3],
+  [4, 5, 6, 7],
+  [8, 9, 10, 11],
+  [12, 13, 14, 15]
+]
+const columns = [
+  [0, 4, 8, 12],
+  [1, 5, 9, 13],
+  [2, 6, 10, 14],
+  [3, 7, 11, 15]
+]
+const areas = [
+  [0, 1, 4, 5],
+  [2, 3, 6, 7],
+  [8, 9, 12, 13],
+  [10, 11, 14, 15]
+]
+
 function Sudoku() {
-  let temp = Array.apply(null, { length: 16 }).map((item, index) => {
-    return {
-      key: index,
-      val: null,
-      isLock: false
-    }
-  })
-  const [arr, setArr] = useState(temp)
-  console.log(setArr)
-  console.log(arr)
+  const [arr, setArr] = useState([])
+  const initSudo = () => {
+    const random = sudoLib[parseInt(Math.random() * sudoLib.length, 10)]
+    setArr(
+      random.map((item, index) => {
+        return {
+          key: index,
+          val: item,
+          isLock: !!item,
+          hasError: false
+        }
+      })
+    )
+  }
+
+  useEffect(() => {
+    initSudo()
+  }, [])
+
+  const handleChange = (cube) => {
+    arr.map((item) => {
+      return item.key === cube.key ? Object.assign(item, cube) : item
+    })
+    setArr(arr)
+  }
+
+  const checkRepeat = (checkList) => {
+    debugger
+    const list = []
+    checkList.forEach((item) => {
+      list.push(arr[item].val)
+    })
+    const temp = new Set(list)
+    return checkList.length === temp.size
+  }
+
+  const handleSubmit = () => {
+    console.log(arr)
+    // 1.0 检测完整性
+    let errArr = []
+    arr.forEach((item) => {
+      if (!item.val) {
+        errArr.push(item.key)
+      }
+    })
+
+    rows.forEach((item) => {
+      const repeat = checkRepeat(item)
+      if (!repeat) {
+        errArr.push(...item)
+      }
+    })
+
+    columns.forEach((item) => {
+      const repeat = checkRepeat(item)
+      if (!repeat) {
+        errArr.push(...item)
+      }
+    })
+
+    areas.forEach((item) => {
+      const repeat = checkRepeat(item)
+      if (!repeat) {
+        errArr.push(...item)
+      }
+    })
+
+    errArr = new Set(errArr)
+
+    setArr(
+      arr.map((item) => {
+        console.log(errArr)
+        return Array.from(errArr).findIndex((o) => o === item.key) > -1
+          ? Object.assign(item, { hasError: true })
+          : Object.assign(item, { hasError: false })
+      })
+    )
+  }
+
+  const handleReset = () => {
+    setArr(
+      arr.map((item) => {
+        return item.isLock ? item : Object.assign(item, { val: null })
+      })
+    )
+  }
+
+  const handleAg = () => {
+    initSudo()
+  }
+
   return (
     <div className={styles['sudoku-wrapper']}>
       <h1>数独</h1>
       <div className={styles['sudo-container']}>
         {arr.map((item) => {
-          return <Cube key={item.key} item={item} />
+          return <Cube key={item.key} item={item} onChange={handleChange} />
         })}
+      </div>
+      <div className={styles['btn-wrapper']}>
+        <Button type="primary" size="large" onClick={handleSubmit}>
+          提交
+        </Button>
+        &nbsp;
+        <Button type="danger" size="large" onClick={handleReset}>
+          重置
+        </Button>
+        &nbsp;
+        <Button type="danger" size="large" onClick={handleAg}>
+          再来一次
+        </Button>
       </div>
     </div>
   )
